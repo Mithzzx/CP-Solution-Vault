8be6553e870f0ee540af8b03bb25b4f5767e91e2
Mithun Rajan <149712505+Mithzzx@users.noreply.github.com>
2025-02-14 13:08:00 +0530

    Created 875. Koko Eating Bananas.py

---
+++ Leetcode/2025/April/875. Koko Eating Bananas.py
@@ -0,0 +1,25 @@
+# Problem: https://leetcode.com/problems/koko-eating-bananas/
+# Difficulty: Medium
+# Approach: Binary Search
+# Time Complexity: O(n * log(max(piles)))
+
+class Solution:
+    def check(self, piles, h, mid):
+        ans = 0
+        for pile in piles:
+            ans += pile // mid
+            if pile % mid != 0:
+                ans += 1
+        return 1 if ans <= h else 0
+
+    def minEatingSpeed(self, piles, h):
+        low = 1
+        high = max(piles)
+
+        while low < high:
+            mid = (low + high) >> 1
+            if self.check(piles, h, mid) == 1:
+                high = mid
+            else:
+                low = mid + 1
+        return low
