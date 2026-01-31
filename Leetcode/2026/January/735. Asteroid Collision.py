5d9dcb15a8e7598b6d35ed112a080a324851d819
Mithun Rajan <149712505+Mithzzx@users.noreply.github.com>
2026-01-31 20:25:53 +0530

    Add solutions for Leetcode problems 150 and 155

---
+++ Leetcode/2026/January/735. Asteroid Collision.py
@@ -0,0 +1,23 @@
+# Problem:https://leetcode.com/problems/asteroid-collision/description/
+# Time Complexity: O(n), Space Complexity: O(n)]
+# Approach: Use a stack to store the asteroids
+# Data Structure: Stack, Algorithm: Stack
+
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+
+        res = []
+
+        for a in asteroids:
+
+            while res and a < 0 < res[-1]:
+                if -a > res[-1]:
+                    res.pop()
+                    continue
+                elif -a == res[-1]:
+                    res.pop()
+                break
+            else:
+                res.append(a)
+
+        return res
